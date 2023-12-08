4d874fc7b77efbd7ea5d0bba9931f9b256464107
Nikita Ridetski <ridetski.nikita@gmail.com>
2023-12-08 16:57:42 +0300

    homework-sprint1

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,38 @@
+type AddressType = {
+    street: string;
+    city: string;
+};
+
+type UserType = {
+    id: number;
+    name: string;
+    age: number;
+    address: AddressType;
+};
+
+type UserListPropsType = {
+    users: UserType[]
+};
+
+export const UserList = (props: UserListPropsType) => {
+    console.log(props)
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
+
+            <ul>
+                {props.users.map( user => (
+                    <li key={user.id} 
+                    id={`hw01-user-${user.id}`}
+                    >
+                        <strong>{user.name}</strong> 
+                        (Age: {user.age})
+                        <strong> Address:</strong>
+                        {user.address.street}, 
+                        {user.address.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
